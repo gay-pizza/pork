3a43b56fcdf71891d34582124fb02cbc83ecccfc
Alex Zenla <alex@fork.run>
2023-09-09 01:51:04 -0400

    evaluator: cache native function resolution

--- evaluator/src/main/kotlin/gay/pizza/pork/evaluator/FunctionContext.kt
+++ evaluator/src/main/kotlin/gay/pizza/pork/evaluator/FunctionContext.kt
@@ -3,18 +3,25 @@ package gay.pizza.pork.evaluator
 import gay.pizza.pork.ast.FunctionDefinition
 
 class FunctionContext(val compilationUnitContext: CompilationUnitContext, val node: FunctionDefinition) : CallableFunction {
+  private fun resolveMaybeNative(): CallableFunction? = if (node.native == null) {
+    null
+  } else {
+    val native = node.native!!
+    val nativeFunctionProvider =
+      compilationUnitContext.evaluator.nativeFunctionProvider(native.form.id)
+    nativeFunctionProvider.provideNativeFunction(native.definition.text)
+  }
+
+  private val nativeCached by lazy { resolveMaybeNative() }
+
   override fun call(arguments: Arguments): Any {
+    if (nativeCached != null) {
+      return nativeCached!!.call(arguments)
+    }
+
     val scope = compilationUnitContext.internalScope.fork()
     for ((index, argumentSymbol) in node.arguments.withIndex()) {
       scope.define(argumentSymbol.id, arguments.values[index])
-    }
-
-    if (node.native != null) {
-      val native = node.native!!
-      val nativeFunctionProvider =
-        compilationUnitContext.evaluator.nativeFunctionProvider(native.form.id)
-      val nativeFunction = nativeFunctionProvider.provideNativeFunction(native.definition.text)
-      return nativeFunction.call(arguments)
     }
 
     if (node.block == null) {
