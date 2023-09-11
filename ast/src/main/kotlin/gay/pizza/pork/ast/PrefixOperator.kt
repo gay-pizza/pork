cdcbdb2bdf3f1e95d34080e45fc2c97ace419dd2
a dinosaur <dino@a-dinosaur.com>
2023-09-11 19:21:09 +1000

    language: add binary operators - unary binary not - infix bitwise and - infix bitwise or - infix bitwise exclusive or (xor)

--- ast/src/main/kotlin/gay/pizza/pork/ast/PrefixOperator.kt
+++ ast/src/main/kotlin/gay/pizza/pork/ast/PrefixOperator.kt
@@ -9,5 +9,6 @@ import kotlinx.serialization.Serializable
 enum class PrefixOperator(val token: String) {
   Negate("!"),
   UnaryPlus("+"),
-  UnaryMinus("-")
+  UnaryMinus("-"),
+  BinaryNot("~")
 }
